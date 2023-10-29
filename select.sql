f545d93c4be77d80ac0f72d7f2c42f465878bf66
vanderleipinto <vandecopinto@gmail.com>
2023-10-29 16:10:35 -0300

    more things on select including IN and BETWEEN

--- select.sql
+++ select.sql
@@ -10,3 +10,8 @@ select * from produtos where (preco = 200) AND (preco < 1000);
 select * from produtos where (preco > 140 AND preco < 1000) OR id_fornecedor = 3;
 -- preferência de colocar as premissas entre parenteses
 
+-- BETWEEN e IN
+SELECT * FROM produtos WHERE estoque BETWEEN 5 AND 10;
+
+SELECT * FROM produtos WHERE id_fornecedor IN (1,6,3);
+-- seleciona baseado em multiplas opções.
