b05a1dd5f8dcd2336ca1fc4ca5e1fd1894176d07
vanderleipinto <vandecopinto@gmail.com>
2023-10-30 16:14:42 -0300

    Added UPDATE and DELETE

--- select.sql
+++ select.sql
@@ -52,7 +52,35 @@ SELECT *, (SELECT fornecedores.nome FROM fornecedores WHERE fornecedores.id = pr
 -- Essa subquery foi usada entre o SELECT e o FROM, a subquery deve retornar somente um resultado.
 -- Possui uma carga maior de processamento pois faz mais consultas que o normal
 
+-- INNER JOIN
 SELECT produtos.id, produtos.nome, produtos.preco, produtos.estoque, produtos.miniestoque, fornecedores.nome  FROM produtos INNER JOIN fornecedores ON fornecedores.id = produtos.id_fornecedor;
 -- INNER JOIN vai juntar as duas tabelas, nesse caso junta os fornecedores através do id do fornecedor em produtos
 SELECT produtos.*, fornecedores.nome AS fornecedor FROM produtos INNER JOIN fornecedores ON fornecedores.id = produtos.id_fornecedor;
+-- Essa é uma forma resumida
 
+-- RIGHT JOIN retorna todos os resultados da tabela da direita, independentemente se ele tem algum resultado na tabela da esquerda. Nesse caso temos o Google como fornecedor e os demais campos ficam com NULL.
+
+-- LEFT JOIN retorna todos os produtos da esquerda independente da tabela da direita ter relação.
+
+-- INNER JOIN só vai pegar o item em que os dois registros baterem. Se tiver um produto sem fornecedor ele não vem. vice versa.
+
+-- INSERT INTO
+INSERT INTO usuarios (nome,datacadastro) VALUES ('Fulano','2020-12-30'), ('Beltrano',NOW());
+
+
+-- UPDATE para alterar algum dado.
+
+UPDATE usuarios SET nome = 'Pedro da Silva' WHERE usuarios.id = 2;
+
+-- 'update sem where'
+-- UPDATE usuarios SET nome = 'Vanderlei';
+-- vai mudar o nome de todos os registros. Para evitar isso digite primeiro
+-- UPDATE usuarios SET WHERE ... ;
+
+
+UPDATE produtos SET preco = preco * 1.1 WHERE id_fornecedor = 6;
+-- Esse comando altera valores de mais de um registro.
+-- Aí digita a condição depois o resto do comando.
+
+DELETE
+DELETE FROM fornecedores WHERE id = 8;
